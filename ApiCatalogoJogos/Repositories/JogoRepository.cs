5b38f1628d4ce406ab390064b243c96352b1ba0d
NatySousa <nataliaspsousa@gmail.com>
2021-06-26 12:24:32 -0300

    Projeto API Catalogo de Jogos

---
+++ ApiCatalogoJogos/Repositories/JogoRepository.cs
@@ -0,0 +1,79 @@
+﻿using ApiCatalogoJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Repositories
+{
+    public class JogoRepository : IJogoRepository
+    {
+        private static Dictionary<Guid, Jogo> jogos = new Dictionary<Guid, Jogo>()
+        {
+            { Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e"), new Jogo { Id = Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e"), Nome = "Fifa 21", Produtora = "EA", Preco = 299} },
+            { Guid.Parse("7c9e6679-7425-40de-944b-e07fc1f90ae7"), new Jogo { Id = Guid.Parse("7c9e6679-7425-40de-944b-e07fc1f90ae7"), Nome = "Fifa 20", Produtora = "EA", Preco = 259} },
+            { Guid.Parse("e3b4248d-c528-4b9c-9fc2-ef5aaeb529e1"), new Jogo { Id = Guid.Parse("e3b4248d-c528-4b9c-9fc2-ef5aaeb529e1"), Nome = "Fifa 19", Produtora = "EA", Preco = 199} },
+            { Guid.Parse("b4dcc3fc-7025-496a-ac28-6cf45bab8737"), new Jogo { Id = Guid.Parse("b4dcc3fc-7025-496a-ac28-6cf45bab8737"), Nome = "Fifa 18", Produtora = "EA", Preco = 159} },
+            { Guid.Parse("53eceea0-cf44-4409-b183-34143782be53"), new Jogo { Id = Guid.Parse("53eceea0-cf44-4409-b183-34143782be53"), Nome = "Elden Rings", Produtora = "Xbox", Preco = 299} },
+            { Guid.Parse("aefc82c8-05ce-4f0e-a064-87fdd9fa9370"), new Jogo { Id = Guid.Parse("aefc82c8-05ce-4f0e-a064-87fdd9fa9370"), Nome = "BFV", Produtora = "Dice", Preco = 199} },
+        };
+
+        public Task<List<Jogo>> Obter(int pagina, int quantidade)
+        {
+            return Task.FromResult(jogos.Values.Skip((pagina -1) * quantidade).Take(quantidade).ToList());
+        }
+
+        public Task<Jogo> Obter(Guid id)
+        {
+            if (!jogos.ContainsKey(id))
+            {
+                return null;
+            }
+
+            return Task.FromResult(jogos[id]);
+        }
+
+        public Task<List<Jogo>> Obter(string nome, string produtora)
+        {
+            return Task.FromResult(jogos.Values.Where(jogo => jogo.Nome.Equals(nome) && jogo.Produtora.Equals(produtora)).ToList());
+        }
+
+        public Task<List<Jogo>> ObterSemLambda(string nome, string produtora)
+        {
+            var retorno = new List<Jogo>();
+
+            foreach (var jogo in jogos.Values)
+            {
+                if (jogo.Nome.Equals(nome) && jogo.Produtora.Equals(produtora))
+                {
+                    retorno.Add(jogo);
+                }
+            }
+
+            return Task.FromResult(retorno);
+        }
+
+        public Task Inserir(Jogo jogo)
+        {
+            jogos.Add(jogo.Id, jogo);
+            return Task.CompletedTask;
+        }
+
+        public Task Atualizar(Jogo jogo)
+        {
+            jogos[jogo.Id] =  jogo;
+            return Task.CompletedTask;
+        }
+
+        public Task Remover(Guid id)
+        {
+            jogos.Remove(id);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            //Fechar conexão do banco
+        }
+    }
+}
---
+++ ApiCatalogoJogos/Repositories/JogoSqlServerRepository.cs
@@ -0,0 +1,138 @@
+﻿using ApiCatalogoJogos.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Repositories
+{
+    public class JogoSqlServerRepository : IJogoRepository
+    {
+        private readonly SqlConnection sqlConnection;
+
+        public JogoSqlServerRepository(IConfiguration configuration)
+        {
+            sqlConnection = new SqlConnection(configuration.GetConnectionString("Default"));
+        }
+
+        public async Task<List<Jogo>> Obter(int pagina, int quantidade)
+        {
+            var jogos = new List<Jogo>();
+
+            var comando = $"select * from Jogos order by id offset {((pagina - 1) * quantidade)} rows fetch next {quantidade} rows only";
+
+            await sqlConnection.OpenAsync();
+            var sqlCommand = new SqlCommand(comando, sqlConnection);
+            var sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+            while (sqlDataReader.Read())
+            {
+                var jogo = new Jogo();
+
+
+                jogo.Id = Guid.Parse(sqlDataReader["Id"].ToString());
+                jogo.Nome = (string)sqlDataReader["Nome"];
+                jogo.Produtora = (string)sqlDataReader["Produtora"];
+                jogo.Preco = double.Parse(sqlDataReader["Preco"].ToString());
+
+                jogos.Add(jogo);
+            }
+
+            await sqlConnection.CloseAsync();
+
+            return jogos;
+        }
+
+        public async Task<Jogo> Obter(Guid id)
+        {
+            Jogo jogo = null;
+            var comando = $"select * from Jogos where Id = '{id}'";
+
+            await sqlConnection.OpenAsync();
+            var sqlCommand = new SqlCommand(comando, sqlConnection);
+            var sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+            while (sqlDataReader.Read())
+            {
+                jogo = new Jogo
+                {
+                    Id = Guid.Parse(sqlDataReader["Id"].ToString()),
+                    Nome = (string)sqlDataReader["Nome"],
+                    Produtora = (string)sqlDataReader["Produtora"],
+                    Preco = double.Parse(sqlDataReader["Preco"].ToString())
+                };
+            }
+
+            await sqlConnection.CloseAsync();
+
+            return jogo;
+        }
+
+        public async Task<List<Jogo>> Obter(string nome, string produtora)
+        {
+            var jogos = new List<Jogo>();
+            var comando = $"select * from Jogos where Nome = '{nome}' and Produtora = '{produtora}'";
+
+            await sqlConnection.OpenAsync();
+            var sqlCommand = new SqlCommand(comando, sqlConnection);
+            var sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+            while (sqlDataReader.Read())
+            {
+                jogos.Add(new Jogo
+                {
+                    Id = Guid.Parse(sqlDataReader["Id"].ToString()),
+                    Nome = (string)sqlDataReader["Nome"],
+                    Produtora = (string)sqlDataReader["Produtora"],
+                    Preco = double.Parse(sqlDataReader["Preco"].ToString())
+                });
+            }
+
+            await sqlConnection.CloseAsync();
+
+            return jogos;
+        }
+
+        public async Task Inserir(Jogo jogo)
+        {
+            var comando = $"insert into Jogos (Id, Nome, Produtora, Preco) Values('{jogo.Id}', '{jogo.Nome}', '{jogo.Produtora}', '{jogo.Preco.ToString()}')";
+
+            await sqlConnection.OpenAsync();
+            var sqlCommand = new SqlCommand(comando, sqlConnection);
+            await sqlCommand.ExecuteNonQueryAsync();
+
+            await sqlConnection.CloseAsync();
+        }
+
+        public async Task Atualizar(Jogo jogo)
+        {
+            var comando = $"update Jogos set Nome = '{jogo.Nome}' , Produtora = '{jogo.Produtora}', Preco = '{jogo.Preco.ToString()}' where Id = '{jogo.Id}'";
+
+            await sqlConnection.OpenAsync();
+            var sqlCommand = new SqlCommand(comando, sqlConnection);
+            await sqlCommand.ExecuteNonQueryAsync();
+
+            await sqlConnection.CloseAsync();
+        }
+
+        public async Task Remover(Guid id)
+        {
+            var comando = $"delete from Jogos where Id = '{id}'";
+
+            await sqlConnection.OpenAsync();
+            var sqlCommand = new SqlCommand(comando, sqlConnection);
+            await sqlCommand.ExecuteNonQueryAsync();
+
+            await sqlConnection.CloseAsync();
+
+        }
+
+        public void Dispose()
+        {
+            sqlConnection?.Close();
+            sqlConnection?.Dispose();
+        }
+    }
+}
